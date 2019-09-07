2b3ae87e6022e9036df617da5dfd0bdaf61943ab
Hugo Hoffmann <hugohoffmann04@gmail.com>
2019-09-07 18:02:20 -0300

    #13 chore: pagination

--- src/app/controllers/AppointmentsController.js
+++ src/app/controllers/AppointmentsController.js
@@ -6,12 +6,15 @@ import File from '../models/File';
 
 class AppointmentsController{
     async index(req, res){
+        const { page = 1 } = req.query;
         const appointments = await Appointments.findAll({
             where: { user_id: req.userId, canceled_at: null },
             order: [
                 'date'
             ],
             attributes: ['id', 'date'],
+            limit: 20,
+            offset: (page - 1) * 20,
             include: [
                 {
                     model: User,
