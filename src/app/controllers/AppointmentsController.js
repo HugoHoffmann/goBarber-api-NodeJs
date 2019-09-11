42ae3462724e5a2dc12602ee5e5254081119b694
Hugo <hugo.hoffmann@nxfacil.com.br>
2019-09-11 14:03:53 -0300

    new: virtual fields in the schedule

--- src/app/controllers/AppointmentsController.js
+++ src/app/controllers/AppointmentsController.js
@@ -16,7 +16,7 @@ class AppointmentsController{
             order: [
                 'date'
             ],
-            attributes: ['id', 'date'],
+            attributes: ['id', 'date', 'past', 'cancelable'],
             limit: 20,
             offset: (page - 1) * 20,
             include: [
